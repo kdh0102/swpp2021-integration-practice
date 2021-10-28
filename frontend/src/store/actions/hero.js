6703e76b4e9a574c817d52ed74be640509a463d5
zz-zye <htht2808@gmail.com>
2021-10-28 19:27:23 +0900

    proxy, csrf token

--- frontend/src/store/actions/hero.js
+++ frontend/src/store/actions/hero.js
@@ -1,30 +1,31 @@
-import * as actionTypes from './actionTypes';
-import { push } from 'connected-react-router';
-import axios from 'axios';
+import * as actionTypes from './actionTypes'
+import { push } from 'connected-react-router'
+import axios from 'axios'
+
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 
 const getHeros_ = (heros) => {
-  return { type: actionTypes.GET_ALL, heros: heros };
-};
+  return { type: actionTypes.GET_ALL, heros: heros }
+}
 
 export const getHeros = () => {
-  return dispatch => {
-    return axios.get('/hero/info/')
-      .then(res => dispatch(getHeros_(res.data)));
-  };
-};
+  return (dispatch) => {
+    return axios.get('/hero/info/').then((res) => dispatch(getHeros_(res.data)))
+  }
+}
 
 const getHero_ = (hero) => {
-  return { type: actionTypes.GET_HERO, target: hero };
-};
+  return { type: actionTypes.GET_HERO, target: hero }
+}
 
 export const getHero = (id) => {
-  return dispatch => {
-    return axios.get('/hero/info/' + id + '/')
-      .then(res => {
-        dispatch(getHero_(res.data))
-      });
-  };
-};
+  return (dispatch) => {
+    return axios.get('/hero/info/' + id + '/').then((res) => {
+      dispatch(getHero_(res.data))
+    })
+  }
+}
 
 const postHero_ = (hero) => {
   return {
@@ -32,18 +33,17 @@ const postHero_ = (hero) => {
     id: hero.id,
     name: hero.name,
     age: hero.age,
-  };
-};
+  }
+}
 
 export const postHero = (hero) => {
   return (dispatch) => {
-    return axios.post('/hero/info/', hero)
-      .then(res => {
-        dispatch(postHero_(res.data));
-        dispatch(push('/heros/'));
-      });
-  };
-};
+    return axios.post('/hero/info/', hero).then((res) => {
+      dispatch(postHero_(res.data))
+      dispatch(push('/heros/'))
+    })
+  }
+}
 
 const putHero_ = (hero) => {
   return {
@@ -51,13 +51,14 @@ const putHero_ = (hero) => {
     id: hero.id,
     name: hero.name,
     age: hero.age,
-  };
-};
+  }
+}
 
 export const putHero = (hero) => {
-  return dispatch => {
-    return axios.put('/hero/info/' + hero.id + '/', hero)
-      .then(res => dispatch(putHero_(hero)))
-      .then(() => dispatch(push('/heros/'+ hero.id)));
-  };
-};
+  return (dispatch) => {
+    return axios
+      .put('/hero/info/' + hero.id + '/', hero)
+      .then((res) => dispatch(putHero_(hero)))
+      .then(() => dispatch(push('/heros/' + hero.id)))
+  }
+}
